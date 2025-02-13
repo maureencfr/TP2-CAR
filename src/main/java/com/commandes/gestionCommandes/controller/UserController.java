42e3240c0a3180a27021e5aba2cfadaacba87552
maureencfr <maureen.coffre@gmail.com>
2025-02-13 13:41:32 +0100

    Gestion de la déconnexion

--- src/main/java/com/commandes/gestionCommandes/controller/UserController.java
+++ src/main/java/com/commandes/gestionCommandes/controller/UserController.java
@@ -1,31 +1,27 @@
 package com.commandes.gestionCommandes.controller;
 
 import com.commandes.gestionCommandes.repository.AccountEntity;
-import com.commandes.gestionCommandes.service.DisconnectService;
 import com.commandes.gestionCommandes.service.FormularLoginService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
     @Autowired
-    private DisconnectService service;
+    private FormularLoginService service;
 
     @PostMapping("/store/home")
     public String displayHomePageAfterDisconnect(){
         return "login";
     }
 
-    @GetMapping("/store/home")
-    public void disconnect(HttpSession session, @PathVariable String id ) {
-        AccountEntity userLogin = (AccountEntity) session.getAttribute("id");
-        if( userLogin == null ) {
-            userLogin = service.findById(id);
-            session.setAttribute("id", userLogin);
-        }
-        userLogin.ajouter(id);
+    @GetMapping("/logout")
+    public RedirectView logout(HttpSession session) {
+        session.invalidate();
+        return new RedirectView("/store/home/login");
     }
 }
